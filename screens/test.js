0f0eb3d959a4429bf8474ae4df6f9c755b0002df
Piyush <pcatjee@gmail.com>
2023-02-11 12:04:29 +0530

    task completed

--- screens/test.js
+++ screens/test.js
@@ -1,11 +1,58 @@
-// {
-//     "jsonrpc":"2.0",
-//     "params": {
-//         "email":"jeetbasak54@gmail.com",
-//     "password":"12345h",
-//     "password_confirmation":"12345h",
-//     "phone":"12345678940",
-//     "first_name":"customer",
-//     "last_name":"das"
-// }
-// }
+{
+    "jsonrpc":"2.0",
+    "params": {
+        "email":"jeetbasak54@gmail.com",
+    "password":"12345h",
+    "password_confirmation":"12345h",
+    "phone":"12345678940",
+    "first_name":"customer",
+    "last_name":"das"
+}
+}
+
+
+
+
+{
+    "jsonrpc":"2.0",
+    "message_1":"verification code is sent to your mail",
+    "results":
+    {
+        "email":"jeetbasak54@gmail.com",
+         "phone":"12345678940",
+         "mobile_otp":410943,
+         {
+             "jsonrpc:"2.0",
+             "params": {
+                   "email":"jeetbasak54@gmail.com",
+    "password":"12345h",
+    "password_confirmation":"12345h",
+    "phone":"12345678940",
+    "first_name":"customer",
+    "last_name":"das"}
+}
+},
+"message_2":"Registration successful"
+}
+
+{
+    "jsonrpc": "2.0",
+    "message_1": "verification code is sent to your mail",
+    "results": {
+        "email": "pcatjee3@gmail.com",
+        "phone": "9891977488",
+        "mobile_otp": 364198,
+    }
+}
+
+
+{
+    "jsonrpc": "2.0",
+    "params": {
+        "mobile": "123456790",
+        "mobile_otp": "491356",
+        "firebase_token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"
+
+
+    }
+}
